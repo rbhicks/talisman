6d7b33f2da1abb4d60f62b8b60dbdfd8d1f5d4a7
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2025-01-09 22:14:59 -0800

    incremental: first functional rev

--- test/athena_test.exs
+++ test/athena_test.exs
@@ -1,10 +1,18 @@
 defmodule AthenaTest do
- use ExSpec, async: true  doctest Athena
+ use ExSpec, async: true
+
+# alias Athena.Test.Support.Fixtures.MissileFactTemplate
 
  describe "fact" do
    context "smoke test" do
      it "actually works" do
-       
+       "uoansteuhosaetuhosaetnuhoaseuthoasutnohausoeuthau" |> IO.puts
+       "uoansteuhosaetuhosaetnuhoaseuthoasutnohausoeuthau" |> IO.puts
+       "uoansteuhosaetuhosaetnuhoaseuthoasutnohausoeuthau" |> IO.puts
+       start_supervised!(Athena.MissileFactTemplate) |> IO.inspect(limit: :infinity)
+       "0394850349583045983405985039485034598304593840593" |> IO.puts
+       "0394850349583045983405985039485034598304593840593" |> IO.puts
+       "0394850349583045983405985039485034598304593840593" |> IO.puts
      end
    end
  end
