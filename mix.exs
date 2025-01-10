7d7c3f1c22154f096ea351799b3498d822a998b5
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2025-01-09 22:10:44 -0800

    added compile path mechanism so test modules will get compiled.

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Athena.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
     ]
   end
@@ -19,6 +20,9 @@ defmodule Athena.MixProject do
     ]
   end
 
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+  
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
