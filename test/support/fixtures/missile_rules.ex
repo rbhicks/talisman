4ef6340731475cbe67d761a97a8f3a1b49b44db7
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2025-01-15 06:26:25 -0800

    added additional rule

--- test/support/fixtures/missile_rules.ex
+++ test/support/fixtures/missile_rules.ex
@@ -1,10 +1,36 @@
 # test missile rules
 defmodule Athen.Test.Support.Fixtures.FoundIcbm do
   alias Athena.Rule
+  alias Athena.Test.Support.Fixtures.MissileFactTemplate
   
   @behaviour Rule
 
-  @lhs_fact_templates [Athena.Test.Support.Fixtures.MissileFactTemplate]
+  @lhs_fact_templates [MissileFactTemplate]
+
+  @impl Rule
+  def add_fact_instance(fact_instance) do
+    {:ok}
+  end
+
+  @impl Rule
+  def evaluate_lhs_for_instances do
+    {:ok, []}
+  end
+
+  @impl Rule
+  def execute_rule do
+    {:ok}
+  end
+end
+
+defmodule Athen.Test.Support.Fixtures.FoundMissileAndBombStrategicAttackVectors do
+  alias Athena.Rule
+  alias Athena.Test.Support.Fixtures.MissileFactTemplate
+  alias Athena.Test.Support.Fixtures.BombFactTemplate
+  
+  @behaviour Rule
+
+  @lhs_fact_templates [MissileFactTemplate, BombFactTemplate]
 
   @impl Rule
   def add_fact_instance(fact_instance) do
