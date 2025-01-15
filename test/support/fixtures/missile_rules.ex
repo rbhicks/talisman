27800d344cd2be001b8da1c610cc1c56449b12a8
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2025-01-15 01:31:56 -0800

    added fact-id

--- test/support/fixtures/missile_rules.ex
+++ test/support/fixtures/missile_rules.ex
@@ -1,1 +1,21 @@
 # test missile rules
+defmodule Athen.Test.Support.Fixtures.FoundIcbm do
+  alias Athena.Rule
+  
+  @behaviour Rule
+
+  @lhs_fact_templates [Athena.Test.Support.Fixtures.MissileFactTemplate]
+
+  @impl Rule
+  def add_fact_instance(fact_instance) do
+    {:ok}
+  end
+
+  # @impl Rule
+  # def evaluate_lhs_for_instances do
+  # end
+
+  # @impl
+  # def execute_rule do
+  # end
+end
