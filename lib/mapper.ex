9712b444f087c96b3ca1ec062252e655304e3271
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2025-02-07 21:51:38 -0800

    incremental

--- lib/mapper.ex
+++ lib/mapper.ex
@@ -5,12 +5,15 @@ defmodule Athena.Mapper do
   # because should a fact template not be
   # referenced by a rule, it's pointless
   # (should be pruned anyway)
-  def add_rule_fact_templates(rule_fact_templates) do
-    GenServer.call(self(), {:add_rule_fact_templates, rule_fact_templates})
+  def add_rule_fact_template(rule_fact_template) do
+    GenServer.call(self(), {:add_rule_fact_template, rule_fact_template})
   end
   #  def create_fact_template_to_rule_lhs_mapping ...
 
-  def handle_call({:add_rule_fact_templates, rule_fact_templates}, _from, state) do
+  def handle_call({:add_rule_fact_template, rule_fact_template}, _from, state) do
+    # the rule_fact_templates will be a list of tuples: {rule_name, fact_templates}
+    # so they can be processed individually. if needed, a batch version can also be
+    # added
     nil
   end
   
