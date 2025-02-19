a90ab841ac59c4906388bcaaa9b98c3f83431a7b
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2025-02-18 20:12:40 -0800

    added more fact templates.

--- test/support/fixtures/fact_templates.ex
+++ test/support/fixtures/fact_templates.ex
@@ -6,3 +6,14 @@ defmodule Athena.Test.Support.Fixtures.BombFactTemplate do
   defstruct name: nil, type: nil, guidance: nil, warhead: nil
 end
 
+defmodule Athena.Test.Support.Fixtures.GuidanceFactTemplate do
+  defstruct name: nil, type: nil, power_source: nil
+end
+
+defmodule Athena.Test.Support.Fixtures.WarheadFactTemplate do
+  defstruct name: nil, type: nil, yield: nil
+end
+
+defmodule Athena.Test.Support.Fixtures.PropulsionFactTemplate do
+  defstruct name: nil, type: nil, power: nil
+end
