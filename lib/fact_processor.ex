9e6487dc4e286f4d0d9ea1968c537ba254093585
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2025-03-04 05:08:07 -0800

    incremental

--- lib/fact_processor.ex
+++ lib/fact_processor.ex
@@ -1,5 +1,47 @@
 defmodule Talisman.FactProcessor do
-#  def assert ...
+  def assert(server, fact_template_name, fact_template) do
+    GenServer.call({:assert, fact_template_name, fact_template})
+    
+  end
+  
 #  def retract ...
 #  def update ...
+
+  def handle_call({:assert, fact_template_name, fact_template}, _from, %{facts: facts} = state) do
+    #!!!!!!!!!111!!!!!!!!!!!!!!!
+    #!!!!!!!!!111!!!!!!!!!!!!!!!
+    #!!!!!!!!!111!!!!!!!!!!!!!!!
+    # need to adjust later when not using the test harness
+    # instead changed to using a dynamic supervisor
+
+    updated_facts = facts
+    |> Map.put("#{fact_template_name}->#{DateTime.utc_now(:microsecond)}", fact_template}
+
+
+    "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" |> IO.puts
+    "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" |> IO.puts
+    "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" |> IO.puts
+    updated_facts |> IO.inspect(limit: :infinity)
+    "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" |> IO.puts
+    "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" |> IO.puts
+    "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" |> IO.puts
+    
+    {
+      :reply,
+      :ok,
+      state
+      |> Map.put(:facts, updated_facts)
+    }
+  end
+
+  def start() do
+    GenServer.start_link(__MODULE__, nil)
+  end
+  
+  def init(_) do
+    {
+      :ok,
+       %{facts: %{}}
+    }
+  end
 end
