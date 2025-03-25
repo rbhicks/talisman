1d1222250ad81e6a285c3877c020f25761cd8156
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2025-03-25 16:10:30 -0700

    incremental: rule function change backout

--- lib/rules.ex
+++ lib/rules.ex
@@ -1,17 +1,22 @@
 defmodule Talisman.Rules do
-  def assert(server, rule_name, rule) do
+  def add_rule(server, rule_name, rule) do
     GenServer.call({:add_rule, rule_name, rule})
     
   end
 
-  def handle_call({:add_rule, rule_name, rule}, _from, %{rules: rules} = state) do
-    #!!!!!!!!!111!!!!!!!!!!!!!!!
-    #!!!!!!!!!111!!!!!!!!!!!!!!!
-    #!!!!!!!!!111!!!!!!!!!!!!!!!
-    # need to adjust later when not using the test harness
-    # instead changed to using a dynamic supervisor
+  def handle_call({:add_rule, rule_name, rule}, _from, %{rules_supervisor: rules_supervisor, rules: rules} = state) do
 
-
+    # updated_rules = ${
+    #   id: rule_name,
+    #   start: {
+    #     Rule,
+    #     :start,
+    #     [
+    #       rule_name,
+          
+    #     ]
+    #   }
+    # }
 
     
     {
@@ -25,10 +30,13 @@ defmodule Talisman.Rules do
     GenServer.start_link(__MODULE__, nil)
   end
   
-  def init(_) do
+  def init({_, rules_supervisor}) do
     {
       :ok,
-       %{facts: %{}}
+       %{
+         rules_supervisor: rules_supervisor,
+         rules: %{}
+       }
     }
   end
 end
