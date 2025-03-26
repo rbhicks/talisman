34defc96f079ca745738a530ee2e51d447eda582
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-03-26 01:08:22 -0700

    implemented powerset creation

--- lib/inference_engine.ex
+++ lib/inference_engine.ex
@@ -1,5 +1,8 @@
 defmodule Talisman.InferenceEngine do
   use GenServer
+
+  alias Talisman.Rule
+  alias Talisman.Rules
   alias Talisman.Utilities
 
 
@@ -11,26 +14,40 @@ defmodule Talisman.InferenceEngine do
 #  defp add_activated_rule ...
 #  defp resolve_execution_order ...
 
+  # we pull the fact templates from rules because the necessary fact templates
+  # can always be derived from the rules' lhs'. if it were done from a collection
+  # of fact templates it could occur that we're process templates that aren't used
+  # by rules which are unnecssary. not meally an opetimization, but also reduces
+  # complexity by not explicitly tracking fact templates. should this become
+  # needed later, we will.
+  def generate_fact_template_name_hashes_powerset(server) do
+    {:ok, fact_template_name_hashes_powerset} = GenServer.call(server, :generate_fact_template_name_hashes_powerset)
+    fact_template_name_hashes_powerset
+  end
 
-  def start(facts_and_rules) do
-    GenServer.start_link(__MODULE__, facts_and_rules)
+  def handle_call(:generate_fact_template_name_hashes_powerset, _from, %{rules: rules} = state) do    
+    {
+      :reply,
+      {
+        :ok,
+        for {_, {_, rule_pid}} <- Rules.get_rules(rules) do
+          Rule.get_lhs_fact_template_names(rule_pid)
+        end
+        |> Enum.flat_map(fn lhs_fact_template_names -> lhs_fact_template_names end)
+        |> Enum.uniq()
+        |> Utilities.generate_powerset()
+      },
+      state}
+  end
+
+  def start(rules) do
+    GenServer.start_link(__MODULE__, rules)
   end
   
-  def init([facts: facts, rules: rules]) do
-
-    "kxxkxkxkxkxkxkxkxkxkxkxkxkxkxkxxkxkxkxkxkkxkxkxkkx" |> IO.puts
-    "kxxkxkxkxkxkxkxkxkxkxkxkxkxkxkxxkxkxkxkxkkxkxkxkkx" |> IO.puts
-    "kxxkxkxkxkxkxkxkxkxkxkxkxkxkxkxxkxkxkxkxkkxkxkxkkx" |> IO.puts
-    facts |> IO.inspect(limit: :infinity)
-    rules |> IO.inspect(limit: :infinity)
-    "32232323232323232323232324234234234234234234234234" |> IO.puts
-    "32232323232323232323232324234234234234234234234234" |> IO.puts
-    "32232323232323232323232324234234234234234234234234" |> IO.puts
-    
+  def init([rules: rules]) do
     {
       :ok,
        %{
-         facts: facts,
          rules: rules
        }
     }
