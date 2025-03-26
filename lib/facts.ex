90a78bb5c9dbb6960aa3a17aceec2a4a7a6379b6
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-03-25 23:58:44 -0700

    created generalized utility function for creating rule and fact creation as they have the same logic

--- lib/facts.ex
+++ lib/facts.ex
@@ -1,5 +1,6 @@
 defmodule Talisman.Facts do
    alias Talisman.Fact
+   alias Talisman.Utilities
    
   def assert(server, fact_template) do
     GenServer.call(server, {:assert, fact_template})
@@ -18,26 +19,24 @@ defmodule Talisman.Facts do
 
     asserted_fact_identity = "#{fact_template_name}->#{DateTime.utc_now(:microsecond)}"
 
-    updated_facts = %{
-      id: asserted_fact_identity,
-      start: {
-        Fact,
-        :start,
-        [
-          fact_template,
-          asserted_fact_identity
-        ]
-      }
-    }
-    |> then(fn asserted_fact_child_spec -> DynamicSupervisor.start_child(facts_supervisor, asserted_fact_child_spec) end)
-    |> then(fn {_, asserted_fact_pid} -> Map.put(facts, asserted_fact_identity, {fact_template_name, asserted_fact_pid}) end)
-
-    {
-      :reply,
-      :ok,
+    Utilities.generate_fact_assertion_or_rule_addition_response(
+      facts_supervisor,
+      %{
+        id: asserted_fact_identity,
+        start: {
+          Fact,
+          :start,
+          [
+            fact_template,
+            asserted_fact_identity
+          ]
+        }
+      },
+      facts,
+      fact_template_name,
+      :facts,
       state
-      |> Map.put(:facts, updated_facts)
-    }
+    )
   end
 
   def handle_call(:get_asserted_facts, _from, %{facts: facts} = state) do
--- lib/rules.ex
+++ lib/rules.ex
@@ -1,5 +1,6 @@
 defmodule Talisman.Rules do
   alias Talisman.Rule
+  alias Talisman.Utilities
   
   def get_rules(server) do
     {:ok, rules} = GenServer.call(server, :get_rules)
@@ -13,28 +14,26 @@ defmodule Talisman.Rules do
 
   def handle_call({:add_rule, rule_name, rule}, _from, %{rules_supervisor: rules_supervisor, rules: rules} = state) do
 
-    updated_rules = %{
-      id: rule_name,
-      start: {
-        Rule,
-        :start,
-        [
-          rule_name,
-          rule.lhs_fact_templates,
-          rule.evaluate_lhs_function,
-          rule.execute_rule_function
-        ]
-      }
-    }
-    |> then(fn rule_child_spec -> DynamicSupervisor.start_child(rules_supervisor, rule_child_spec) end)
-    |> then(fn {_, rule_pid} -> Map.put(rules, rule_name, {rule_name, rule_pid}) end)
-    
-    {
-      :reply,
-      :ok,
+    Utilities.generate_fact_assertion_or_rule_addition_response(
+      rules_supervisor,
+      %{
+        id: rule_name,
+        start: {
+          Rule,
+          :start,
+          [
+            rule_name,
+            rule.lhs_fact_templates,
+            rule.evaluate_lhs_function,
+            rule.execute_rule_function
+          ]
+        }
+      },
+      rules,
+      rule_name,
+      :rules,
       state
-      |> Map.put(:rules, updated_rules)
-    }
+    )    
   end
 
   def handle_call(:get_rules, _from, %{rules: rules} = state) do
