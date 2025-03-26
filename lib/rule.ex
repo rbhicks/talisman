1c8d2ec132b7425c7addac4285faa004caf4d493
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2025-03-25 17:14:12 -0700

    added access api for lhs fact templates

--- lib/rule.ex
+++ lib/rule.ex
@@ -1,6 +1,11 @@
 defmodule Talisman.Rule do
   use GenServer
 
+  def get_lhs_fact_templates(rule_pid) do
+    {:ok, lhs_fact_templates} = GenServer.call(rule_pid, :get_lhs_fact_templates)
+    lhs_fact_templates
+  end
+  
   def add_fact_id(fact_id) do
     {:ok}
   end
@@ -11,6 +16,17 @@ defmodule Talisman.Rule do
   
   def execute_rule do
     {:ok}
+  end
+
+  def handle_call(:get_lhs_fact_templates, _from, {get_lhs_fact_templates_function, _, _, _} = state) do
+    {
+      :reply,
+      {
+        :ok,
+        get_lhs_fact_templates_function.()
+      },
+      state
+    }
   end
 
   def start(rule_name, get_lhs_fact_templates_function, evaluate_lhs_function, execute_rule_function) do
