f4d9079967d685803fc63eaf7ea61ab72deb0304
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-04-24 02:01:50 -0700

    set up facts to create fact_template_names and asserts facts mapping.

--- lib/facts.ex
+++ lib/facts.ex
@@ -2,10 +2,11 @@ defmodule Talisman.Facts do
   use GenServer
   
   alias Talisman.Fact
-   alias Talisman.Utilities
+  alias Talisman.Utilities
+  alias Talisman.Mapper
    
-  def assert(server, fact_template) do
-    GenServer.call(server, {:assert, fact_template})
+  def assert(server, fact_template, mapper) do
+    GenServer.call(server, {:assert, fact_template, mapper})
   end
   
 #  def retract ...
@@ -17,11 +18,11 @@ defmodule Talisman.Facts do
     asserted_facts
   end
 
-  def handle_call({:assert, %fact_template_name{} = fact_template}, _from, %{facts_supervisor: facts_supervisor, facts: facts} = state) do
+  def handle_call({:assert, %fact_template_name{} = fact_template, mapper}, _from, %{facts_supervisor: facts_supervisor, facts: facts} = state) do
 
     asserted_fact_identity = "#{fact_template_name}->#{DateTime.utc_now(:microsecond)}"
 
-    Utilities.generate_fact_assertion_or_rule_addition_response(
+    response = Utilities.generate_fact_assertion_or_rule_addition_response(
       facts_supervisor,
       %{
         id: asserted_fact_identity,
@@ -39,6 +40,12 @@ defmodule Talisman.Facts do
       :facts,
       state
     )
+
+    {_, {_, pid}, _} = response
+
+    Mapper.create_fact_template_name_asserted_facts_mapping(mapper, fact_template_name, pid)
+    
+    response
   end
 
   def handle_call(:get_asserted_facts, _from, %{facts: facts} = state) do
