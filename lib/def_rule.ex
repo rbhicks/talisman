3401a664a96a7da7ef237473ff5b1464aedbb27b
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-05-23 13:46:18 -0700

    incremental: cracking the ast elements

--- lib/def_rule.ex
+++ lib/def_rule.ex
@@ -1,12 +1,25 @@
 defmodule DefRule do
 
+  def crack_fact_template_names({_, _, [{_, _, [[]|_]}|_]}), do: []
+  def crack_fact_template_names({_, _, [{_, _, [fact_template_names|_]}|_]}), do: fact_template_names
 
-  defmacro def_rule(rule) do
+  # def crack_lhs_evaluation_and_rhs_execution_bodies({_, _, [{_, _, lhs_evaluation_and_rhs_execution_bodies}|_]}) do
+  def crack_lhs_evaluation_and_rhs_execution_bodies({_, _, [{_, _, lhs_evaluation_and_rhs_execution_bodies}|_]}) do
+    lhs_evaluation_and_rhs_execution_bodies
+  end
+
+  defmacro def_rule(rule_name, rule) do
     quote do
       unquote("(*&(*&(*&(*&(*&(*&(*&(*&(*&(*" |> IO.puts)
       unquote("(*&(*&(*&(*&(*&(*&(*&(*&(*&(*" |> IO.puts)
       unquote("(*&(*&(*&(*&(*&(*&(*&(*&(*&(*" |> IO.puts)
+      unquote(rule_name |> IO.inspect(limit: :infinity))
+      unquote("=================================" |> IO.puts())
       unquote(rule |> IO.inspect(limit: :infinity))
+      unquote("=================================" |> IO.puts())
+      unquote(rule |> crack_fact_template_names() |> IO.inspect(limit: :infinity))
+      unquote("=================================" |> IO.puts())
+      unquote(rule |> crack_lhs_evaluation_and_rhs_execution_bodies() |> IO.inspect(limit: :infinity))
       unquote("kwm;qbjqjvwkmbqjvkm;qjk;qjk;q" |> IO.puts)
       unquote("kwm;qbjqjvwkmbqjvkm;qjk;qjk;q" |> IO.puts)
       unquote("kwm;qbjqjvwkmbqjvkm;qjk;qjk;q" |> IO.puts)
