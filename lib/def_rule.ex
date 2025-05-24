cdcc2297b436e95b65b4280bf6fb66ed52a825f8
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-05-24 13:16:14 -0700

    incremental: changing macro semantics and emitting

--- lib/def_rule.ex
+++ lib/def_rule.ex
@@ -15,53 +15,39 @@ defmodule DefRule do
     fact_template_name
   end
   
-  def crack_fact_template_names({_, _, [{_, _, [[]|_]}|_]}), do: []
-  def crack_fact_template_names({_, _, [{_, _, [fact_template_names_ast|_]}|_]}) do    
+  def crack_fact_template_names_from_ast_node({_, _, [{_, _, [[]|_]}|_]}), do: []
+  def crack_fact_template_names_from_ast_node({_, _, [{_, _, [fact_template_names_ast|_]}|_]}) do    
     fact_template_names_ast
     |> Enum.map(fn fact_template_name_ast_node ->
       crack_fact_template_name_from_ast_node(fact_template_name_ast_node)
     end)
   end
 
-  def crack_lhs_evaluation_and_rhs_execution_bodies({_, _, [{_, _, [_|[{_, _, [lhs_evaluation_body|rhs_execution_body]}|_]]}|_]}) do
-    {lhs_evaluation_body, rhs_execution_body}
+  def crack_function_info_from_ast_node({:fn, _, [{:->, _, [closure_function_head|[{_, _, [lhs_evaluation_body|rhs_execution_body]}|_]]}|_]}) do
+    {closure_function_head, lhs_evaluation_body, rhs_execution_body}
   end
 
   defmacro def_rule(rule_name, rule) do
 
-    fact_template_names = crack_fact_template_names(rule)
-    {lhs_evaluation_body, rhs_execution_body} = crack_lhs_evaluation_and_rhs_execution_bodies(rule)
+    fact_template_names = crack_fact_template_names_from_ast_node(rule)
+    {closure_function_head, lhs_evaluation_body, rhs_execution_body} = crack_function_info_from_ast_node(rule)
     unique_fact_template_names = fact_template_names
     |> Enum.uniq
     fact_template_name_frequencies = fact_template_names
     |> Enum.frequencies
-    
 
-    "asnoetuhasoentuhasoentuh" |> IO.puts()
-    "asnoetuhasoentuhasoentuh" |> IO.puts()
-    "asnoetuhasoentuhasoentuh" |> IO.puts()
-    rule_name |> IO.inspect(limit: :infinity)
-    # "=================================" |> IO.puts()
-    # fact_template_names |> IO.inspect(limit: :infinity)
-    # "=================================" |> IO.puts()
-    unique_fact_template_names |> IO.inspect(limit: :infinity)
-    "=================================" |> IO.puts()
-    fact_template_name_frequencies |> IO.inspect(limit: :infinity)
-    "48573495873459873459348"  |> IO.puts()
-    "48573495873459873459348"  |> IO.puts()
-    "48573495873459873459348"  |> IO.puts()
+    {:fn, [],
+     [
+       {:->, [],
+       [
+         closure_function_head,
+         {
+           lhs_evaluation_body,
+           rhs_execution_body
+         }
+       ]}
+     ]}
 
-    quote do
-      unquote("(*&(*&(*&(*&(*&(*&(*&(*&(*&(*" |> IO.puts)
-      unquote("(*&(*&(*&(*&(*&(*&(*&(*&(*&(*" |> IO.puts)
-      unquote("(*&(*&(*&(*&(*&(*&(*&(*&(*&(*" |> IO.puts)
-      unquote(lhs_evaluation_body |> IO.inspect(limit: :infinity))
-      unquote("=================================" |> IO.puts())
-      unquote(rhs_execution_body |> IO.inspect(limit: :infinity))
-      unquote("kwm;qbjqjvwkmbqjvkm;qjk;qjk;q" |> IO.puts)
-      unquote("kwm;qbjqjvwkmbqjvkm;qjk;qjk;q" |> IO.puts)
-      unquote("kwm;qbjqjvwkmbqjvkm;qjk;qjk;q" |> IO.puts)
-    end
   end
 end
 
