80f175305621f5cc930c40a88db4eae9922ce2e7
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-05-30 18:50:21 -0700

    incremental

--- lib/rule.ex
+++ lib/rule.ex
@@ -58,7 +58,7 @@ defmodule Talisman.Rule do
         _from,
         # %{evaluate_lhs_function_args: evaluate_lhs_function_args,
         #   evaluate_lhs_function: evaluate_lhs_function} = state
-        %{get_rule_lhs_evaluation_and_rhs_execution_functions: get_rule_lhs_evaluation_and_rhs_execution_functions} = state
+        %{rule_name: rule_name, get_rule_lhs_evaluation_and_rhs_execution_functions: get_rule_lhs_evaluation_and_rhs_execution_functions} = state
       ) do
     #    use apply/2 with lhs_fact_template_names
 
@@ -69,21 +69,42 @@ defmodule Talisman.Rule do
     # fact_instance = Fact.get_fact_instance(asserted_fact_pid)
     
     # {lhs_evaluation_function, rhs_execution_function} = get_rule_lhs_evaluation_and_rhs_execution_functions.(fact_instance)
-    "m8v823x0ks6d9eh23j4v9u8uyy7w9t" |> IO.puts
-    "m8v823x0ks6d9eh23j4v9u8uyy7w9t" |> IO.puts
-    "m8v823x0ks6d9eh23j4v9u8uyy7w9t" |> IO.puts
-    asserted_facts |> IO.inspect(limit: :infinity)
-    # "==============================" |> IO.puts
-    # asserted_fact_pid |> IO.inspect(limit: :infinity)
-    # "==============================" |> IO.puts
-    # fact_instance |> IO.inspect(limit: :infinity)
-    # "==============================" |> IO.puts
-    # lhs_evaluation_function.() |> IO.inspect(limit: :infinity)
-    # "==============================" |> IO.puts
-    # rhs_execution_function.() |> IO.inspect(limit: :infinity)
-    "uwa901j4uhb4aadmocdrbg1w9hmqez" |> IO.puts
-    "uwa901j4uhb4aadmocdrbg1w9hmqez" |> IO.puts
-    "uwa901j4uhb4aadmocdrbg1w9hmqez" |> IO.puts
+
+    for closure_function_fact_params <- asserted_facts do
+      "m8v823x0ks6d9eh23j4v9u8uyy7w9t" |> IO.puts
+      "m8v823x0ks6d9eh23j4v9u8uyy7w9t" |> IO.puts
+      "m8v823x0ks6d9eh23j4v9u8uyy7w9t" |> IO.puts
+      rule_name |> IO.inspect(limit: :infinity)
+      "==============================" |> IO.puts
+      params = for {_, asserted_fact_pid} <- closure_function_fact_params do
+#        apply(get_rule_lhs_evaluation_and_rhs_execution_functions, Fact.get_fact_instance(asserted_fact_pid))
+        Fact.get_fact_instance(asserted_fact_pid)
+      end
+
+      
+      get_rule_lhs_evaluation_and_rhs_execution_functions
+      |> Function.info()
+      |> Keyword.get(:arity)
+      |> IO.inspect(limit: :infinity)
+      "==============================" |> IO.puts
+      params |> IO.inspect(limit: :infinity)
+      "==============================" |> IO.puts
+      {lhs_evaluation_function, rhs_execution_function} = apply(get_rule_lhs_evaluation_and_rhs_execution_functions, params)
+#      |> IO.inspect(limit: :infinity)
+      "==============================" |> IO.puts
+#      asserted_facts |> IO.inspect(limit: :infinity)
+      # "==============================" |> IO.puts
+      # asserted_fact_pid |> IO.inspect(limit: :infinity)
+      # "==============================" |> IO.puts
+      # fact_instance |> IO.inspect(limit: :infinity)
+      # "==============================" |> IO.puts
+      lhs_evaluation_function.() |> IO.inspect(limit: :infinity)
+      "==============================" |> IO.puts
+      rhs_execution_function.() |> IO.inspect(limit: :infinity)
+      "uwa901j4uhb4aadmocdrbg1w9hmqez" |> IO.puts
+      "uwa901j4uhb4aadmocdrbg1w9hmqez" |> IO.puts
+      "uwa901j4uhb4aadmocdrbg1w9hmqez" |> IO.puts
+    end
     active? = false
     
     #    evaluate_lhs_function.(asserted_facts)
@@ -111,8 +132,11 @@ defmodule Talisman.Rule do
       __MODULE__,
       # {lhs_fact_template_names, lhs_fact_multiplicity, evaluate_lhs_function_args,
       #  evaluate_lhs_function, execute_rule_function},
-      {lhs_fact_template_names, lhs_fact_multiplicity,
-       get_rule_lhs_evaluation_and_rhs_execution_functions},
+      {
+        rule_name,
+        lhs_fact_template_names, lhs_fact_multiplicity,
+        get_rule_lhs_evaluation_and_rhs_execution_functions
+      },
       name: {:global, rule_name}
     )
   end
@@ -120,12 +144,14 @@ defmodule Talisman.Rule do
   def init(
         # {lhs_fact_template_names, lhs_fact_multiplicity, evaluate_lhs_function_args,
         #  evaluate_lhs_function, execute_rule_function}
-        {lhs_fact_template_names, lhs_fact_multiplicity,
+        {rule_name,
+          lhs_fact_template_names, lhs_fact_multiplicity,
          get_rule_lhs_evaluation_and_rhs_execution_functions}
       ) do
     {
       :ok,
       %{
+        rule_name: rule_name, 
         lhs_fact_template_names: lhs_fact_template_names,
         lhs_fact_multiplicity: lhs_fact_multiplicity,
         # evaluate_lhs_function_args: evaluate_lhs_function_args,
