3544c5066534d50a82dcdc2fe845aa6649cddcd2
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-05-30 19:01:21 -0700

    incremental: new baseline

--- lib/rule.ex
+++ lib/rule.ex
@@ -56,68 +56,41 @@ defmodule Talisman.Rule do
   def handle_call(
         {:evaluate_lhs_for_asserted_facts, asserted_facts},
         _from,
-        # %{evaluate_lhs_function_args: evaluate_lhs_function_args,
-        #   evaluate_lhs_function: evaluate_lhs_function} = state
         %{
           rule_name: rule_name,
           get_rule_lhs_evaluation_and_rhs_execution_functions:
             get_rule_lhs_evaluation_and_rhs_execution_functions
         } = state
       ) do
-    #    use apply/2 with lhs_fact_template_names
-
-    # {_, asserted_fact_pid} = asserted_facts
-    # |> List.first
-    # |> List.first
-
-    # fact_instance = Fact.get_fact_instance(asserted_fact_pid)
-
-    # {lhs_evaluation_function, rhs_execution_function} = get_rule_lhs_evaluation_and_rhs_execution_functions.(fact_instance)
-
-    for closure_function_fact_params <- asserted_facts do
+    for closure_function_param_info <- asserted_facts do
       "m8v823x0ks6d9eh23j4v9u8uyy7w9t" |> IO.puts()
       "m8v823x0ks6d9eh23j4v9u8uyy7w9t" |> IO.puts()
       "m8v823x0ks6d9eh23j4v9u8uyy7w9t" |> IO.puts()
-      rule_name |> IO.inspect(limit: :infinity)
-      "==============================" |> IO.puts()
-
-      params =
-        for {_, asserted_fact_pid} <- closure_function_fact_params do
-          #        apply(get_rule_lhs_evaluation_and_rhs_execution_functions, Fact.get_fact_instance(asserted_fact_pid))
+      closure_function_params =
+        for {_, asserted_fact_pid} <- closure_function_param_info do
           Fact.get_fact_instance(asserted_fact_pid)
         end
 
-      get_rule_lhs_evaluation_and_rhs_execution_functions
-      |> Function.info()
-      |> Keyword.get(:arity)
-      |> IO.inspect(limit: :infinity)
-
-      "==============================" |> IO.puts()
-      params |> IO.inspect(limit: :infinity)
-      "==============================" |> IO.puts()
-
       {lhs_evaluation_function, rhs_execution_function} =
-        apply(get_rule_lhs_evaluation_and_rhs_execution_functions, params)
+        apply(get_rule_lhs_evaluation_and_rhs_execution_functions, closure_function_params)
 
-      #      |> IO.inspect(limit: :infinity)
-      "==============================" |> IO.puts()
-      #      asserted_facts |> IO.inspect(limit: :infinity)
-      # "==============================" |> IO.puts
-      # asserted_fact_pid |> IO.inspect(limit: :infinity)
-      # "==============================" |> IO.puts
-      # fact_instance |> IO.inspect(limit: :infinity)
-      # "==============================" |> IO.puts
-      lhs_evaluation_function.() |> IO.inspect(limit: :infinity)
-      "==============================" |> IO.puts()
-      rhs_execution_function.() |> IO.inspect(limit: :infinity)
+      lhs_evaluation_function.()
+      rhs_execution_function.()
       "uwa901j4uhb4aadmocdrbg1w9hmqez" |> IO.puts()
       "uwa901j4uhb4aadmocdrbg1w9hmqez" |> IO.puts()
       "uwa901j4uhb4aadmocdrbg1w9hmqez" |> IO.puts()
     end
 
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # need to return a map or array of activations for each
+    # set of fact istances
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
     active? = false
-
-    #    evaluate_lhs_function.(asserted_facts)
 
     {
       :reply,
