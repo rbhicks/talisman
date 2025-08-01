8eba1cb4c097bd107dd43e9bb31fa37135e916bf
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-07-31 22:10:16 -0700

    removed old cartesian product implementation.

--- lib/talisman.ex
+++ lib/talisman.ex
@@ -7,11 +7,4 @@ defmodule Talisman do
     children = []
     Supervisor.start_link(children, strategy: :one_for_one)
   end
-
-  def cartesian_product(lists) do
-    lists
-    |> Enum.reduce([[]], fn list, acc ->
-      for x <- acc, y <- list, do: x ++ [y]
-    end)
-  end
 end
