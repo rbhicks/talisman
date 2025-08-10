42e3c607b17a6e003bc5ba27943805f25118dcd2
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-08-09 20:00:28 -0700

    incremental

--- lib/talisman.ex
+++ lib/talisman.ex
@@ -37,6 +37,8 @@ defmodule Talisman do
 
     {:ok, facts_supervisor_pid} = TalismanDynamicSupervisor.start_talisman_genserver(facts_supervisor_child_spec)
     {:ok, rules_supervisor_pid} = TalismanDynamicSupervisor.start_talisman_genserver(rules_supervisor_child_spec)
+
+    {:ok, _} = Registry.register(Talisman.Registry, :facts_supervisor, facts_supervisor_pid)
     
     "dijfd83gi156niost84s7xokwuu9d7" |> IO.puts
     "dijfd83gi156niost84s7xokwuu9d7" |> IO.puts
@@ -46,6 +48,8 @@ defmodule Talisman do
     facts_supervisor_pid |> IO.inspect(limit: :infinity)
     "==============================" |> IO.puts
     rules_supervisor_pid |> IO.inspect(limit: :infinity)
+    "==============================" |> IO.puts
+    Registry.lookup(Talisman.Registry, :facts_supervisor) |> IO.inspect(limit: :infinity)
     "9grcvmtv8d0b2xklwzbqaamtl02lr5" |> IO.puts
     "9grcvmtv8d0b2xklwzbqaamtl02lr5" |> IO.puts
     "9grcvmtv8d0b2xklwzbqaamtl02lr5" |> IO.puts
