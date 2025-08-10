10f97846a1084711d6c9faf5e7a9f2b3ff4efacc
Ryan Bryce Hicks <r.b.hicks@gmail.com>
2025-08-09 19:20:06 -0700

    incremental

--- lib/talisman.ex
+++ lib/talisman.ex
@@ -4,16 +4,24 @@ defmodule Talisman do
   """
 
   def start(_type, _args) do
-
-    "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" |> IO.puts
-    "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" |> IO.puts
-    "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" |> IO.puts
-    "add all the children now that the internal refs are dynamic." |> IO.puts
-    "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" |> IO.puts
-    "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" |> IO.puts
-    "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" |> IO.puts
     
-    children = []
-    Supervisor.start_link(children, strategy: :one_for_one)
+    children = [
+      Talisman.TalismanDynamicSupervisor,
+      {Registry, keys: :unique, name: Talisman.Registry}
+    ]
+    
+    {:ok, supervisor_id} = Supervisor.start_link(children, strategy: :one_for_one)
+
+    "dijfd83gi156niost84s7xokwuu9d7" |> IO.puts
+    "dijfd83gi156niost84s7xokwuu9d7" |> IO.puts
+    "dijfd83gi156niost84s7xokwuu9d7" |> IO.puts
+    supervisor_id |> IO.inspect(limit: :infinity)
+    "==============================" |> IO.puts
+    Supervisor.which_children(supervisor_id) |> IO.inspect(limit: :infinity)
+    "9grcvmtv8d0b2xklwzbqaamtl02lr5" |> IO.puts
+    "9grcvmtv8d0b2xklwzbqaamtl02lr5" |> IO.puts
+    "9grcvmtv8d0b2xklwzbqaamtl02lr5" |> IO.puts
+
+    {:ok, supervisor_id}
   end
 end
